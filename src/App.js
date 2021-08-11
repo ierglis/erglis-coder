ebb89f7cbba765d9f922ced4c78c0f8ef9a1801a
ierglis <juanierglis@gmail.com>
2021-08-10 23:25:13 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { NavBar } from "./components/NavBar/NavBar";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
+
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <ItemListContainer/>
     </div>
   );
 }
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ItemListContainer = () => {
+    return(
+        <div>
+            <h1>HOLA MUNDO</h1>
+        </div>
+
+    )
+}
